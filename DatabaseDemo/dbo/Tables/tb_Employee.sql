2079370d7a1edc7b10ebb3aa7fa54bc5553719d7
Kumar <skumar2118@dxc.com>
2019-09-02 15:40:27 +0530

    Add project files.

---
+++ DatabaseDemo/dbo/Tables/tb_Employee.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tb_Employee]
+(
+	[ID] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(100) NULL, 
+    [Address] NCHAR(500) NULL, 
+    [City] NCHAR(100) NULL, 
+    [Mobile] INT NULL
+)
+
+GO
+
+CREATE INDEX [IX_tb_Employee_ID] ON [dbo].[tb_Employee] ([ID])
