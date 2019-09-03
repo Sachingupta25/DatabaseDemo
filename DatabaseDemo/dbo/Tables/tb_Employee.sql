26c812cffd9cd5581c6e0b7ad0bb8a8712994a00
Sachin Kumar <skumar2118@dxc.com>
2019-09-03 14:09:42 +0530

    Added table tb_Project

--- DatabaseDemo/dbo/Tables/tb_Employee.sql
+++ DatabaseDemo/dbo/Tables/tb_Employee.sql
@@ -1,13 +1,21 @@
 ﻿CREATE TABLE [dbo].[tb_Employee]
 (
 	[ID] INT NOT NULL PRIMARY KEY, 
-    [Name] NCHAR(100) NULL, 
-    [Address] NCHAR(500) NULL, 
-    [City] NCHAR(100) NULL,
-	[Mobile] INT NULL
+    [Name] NVARCHAR(100) NULL, 
+    [Address] NVARCHAR(500) NULL, 
+    [City] NVARCHAR(100) NULL,
+	[Mobile] NVARCHAR NULL,
+	[PID] INT NULL
     
 )
 
 GO
 
 CREATE INDEX [IX_tb_Employee_ID] ON [dbo].[tb_Employee] ([ID])
+
+GO
+
+
+
+
+
---
+++ DatabaseDemo/dbo/Tables/tb_Project.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[tb_Project]
+(	
+	[PID] INT NOT NULL PRIMARY KEY, 
+	[ProjectCode] NVARCHAR(50),
+    [ProjectName] NVARCHAR(200) NULL, 
+    [StartDate] DATE,
+	[EndDate] DATE,
+    [ProjectGroup] NVARCHAR(100) NULL,
+	[ProjectCycle] NVARCHAR(100) NULL,
+	[EMPID] INT CONSTRAINT FK_Project_ID FOREIGN KEY (EMPID)
+        REFERENCES tb_Employee(ID)
+    
+)
+
+GO
